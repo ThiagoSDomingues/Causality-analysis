e3c7630a782b23f649860eaf3fec564a2c5245fa
Thiago Siqueira Domingues <68281601+ThiagoSDomingues@users.noreply.github.com>
2026-01-04 16:53:46 -0300

    Create causality_analysis.py for simulation analysis
    Added a script for analyzing causality conditions in heavy-ion collision simulations.

---
+++ analysis_scripts/causality_analysis.py
@@ -0,0 +1,10 @@
+### Author: OptimusThi
+
+import necessary_conditions
+import sufficient_conditions
+import numpy as np
+
+"""
+Script to analyze causality conditions in simulations of heavy-ion collisions.
+"""
+
